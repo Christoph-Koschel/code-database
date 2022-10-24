6986dfbb1bae0863ecfcfff826b1abf5ab844372
Christoph Koschel <christoph.koschel@gmail.com>
2022-10-24 20:17:21 +0200

    Added Math module

---
+++ src/math.ts
@@ -0,0 +1,18 @@
+import {float, int} from "./symbols";
+
+export function rand(min: int, max: int): int {
+    return Math.floor(Math.random() * (max - min)) + max as int;
+}
+
+export function randF(min:float, max: float): float {
+    return Math.floor(Math.random() * (max - min)) + max;
+}
+
+export function isInt(num: number): boolean {
+    return num % 1 == 0;
+}
+
+export function isFloat(num: number): boolean {
+    return !isInt(num);
+}
+
